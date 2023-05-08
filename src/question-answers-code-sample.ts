5e0f917566a5153a410a7ef73bb02527cdbd49e0
Shekh Rasel Masrur Ahmmad Nissan <shekhraselmasrurahmmadnissan@gmail.com>
2023-05-08 21:59:18 +0600

    QnA

---
+++ src/question-answers-code-sample.ts
@@ -0,0 +1,14 @@
+// const genericFunction = <T>(value: T): T => {
+// 	return value;
+// };
+
+// const test1 = genericFunction<string>('Rasel');
+// const test2 = genericFunction<number>(1002);
+// console.log(test1);
+// console.log(test2);
+
+enum Cars {
+	Honda = 'Honda',
+	Hyundai = 'Hyundai',
+	Volvo = 'Volvo',
+}
