fd36ba82fc70a8772d1e368e835db946c69da0f1
Shekh Rasel Masrur Ahmmad Nissan <shekhraselmasrurahmmadnissan@gmail.com>
2023-05-08 14:12:51 +0600

    Assignment Problem 1 and 2 complete.

---
+++ src/assignment-problem-1.ts
@@ -0,0 +1,13 @@
+/**
+ * Create a TypeScript program that declares a function that takes two parameters, a string and a number. The function should log the string parameter repeated the number of times specified by the number parameter. If the number parameter is not provided, it should default to 3.
+ */
+
+// type FunctionType = (text: string, count?: number) => void;
+
+// const consoleMultiple: FunctionType = (text, count = 3) => {
+// 	for (let i = 0; i < count; i++) {
+// 		console.log(text);
+// 	}
+// };
+
+// consoleMultiple('Rasel', 7);
---
+++ src/assignment-problem-2.ts
@@ -0,0 +1,25 @@
+/**
+ * Write a function that takes in an array of objects with properties name and age, and returns only the objects where the person's age is greater than or equal to 18,
+ */
+
+// interface IPerson {
+// 	name: string;
+// 	age: number;
+// }
+// type FunctionType = <T extends IPerson, k extends keyof T>(
+// 	persons: Array<T>
+// ) => Array<T>;
+
+// const checkAdult: FunctionType = (persons) => {
+// 	return persons.filter((person) => person.age >= 18);
+// };
+
+// const persons: Array<IPerson> = [
+// 	{ name: 'Rasel', age: 25 },
+// 	{ name: 'Nissan', age: 16 },
+// 	{ name: 'Siam', age: 24 },
+// 	{ name: 'Rayan', age: 10 },
+// ];
+
+// const result = checkAdult(persons);
+// console.log(result);
